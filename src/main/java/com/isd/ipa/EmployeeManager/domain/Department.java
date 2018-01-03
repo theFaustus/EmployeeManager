339373be3c56e8cde1e579b706b65490a35c95aa
Faust <jhoonnyc@gmail.com>
2018-01-03 14:46:10 +0200

    Added the models

---
+++ src/main/java/com/isd/ipa/EmployeeManager/domain/Department.java
@@ -0,0 +1,89 @@
+package com.isd.ipa.EmployeeManager.domain;
+
+import java.util.List;
+
+/**
+ * Created by Faust on 1/3/2018.
+ */
+public class Department {
+    private String departmentId;
+    private String name;
+    private String description;
+    private List<Employee> employees;
+
+    public Department() {
+    }
+
+    public Department(String name, String description, List<Employee> employees) {
+        this.name = name;
+        this.description = description;
+        this.employees = employees;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Department that = (Department) o;
+
+        if (getDepartmentId() != null ? !getDepartmentId().equals(that.getDepartmentId()) : that.getDepartmentId() != null)
+            return false;
+        if (getName() != null ? !getName().equals(that.getName()) : that.getName() != null) return false;
+        if (getDescription() != null ? !getDescription().equals(that.getDescription()) : that.getDescription() != null)
+            return false;
+        return getEmployees() != null ? getEmployees().equals(that.getEmployees()) : that.getEmployees() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getDepartmentId() != null ? getDepartmentId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getDescription() != null ? getDescription().hashCode() : 0);
+        result = 31 * result + (getEmployees() != null ? getEmployees().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departmentId='" + departmentId + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", employees=" + employees +
+                '}';
+    }
+}
---
+++ src/main/java/com/isd/ipa/EmployeeManager/domain/Employee.java
@@ -0,0 +1,62 @@
+package com.isd.ipa.EmployeeManager.domain;
+
+/**
+ * Created by Faust on 1/3/2018.
+ */
+public class Employee {
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId='" + employeeId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
