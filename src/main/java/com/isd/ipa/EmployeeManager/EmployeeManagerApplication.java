d5373863b2f50cb355af11546ed94424869dc8a1
Faust <jhoonnyc@gmail.com>
2018-01-03 14:28:30 +0200

    Created the project skeleton

---
+++ src/main/java/com/isd/ipa/EmployeeManager/EmployeeManagerApplication.java
@@ -0,0 +1,12 @@
+package com.isd.ipa.EmployeeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/isd/ipa/EmployeeManager/EmployeeManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.isd.ipa.EmployeeManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
